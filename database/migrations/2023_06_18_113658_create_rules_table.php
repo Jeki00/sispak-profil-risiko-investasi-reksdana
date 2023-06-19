6c3d1c30947ac5fea839ea86b11bc32eb624451b
Zaky Basith <zakybasith38@gmail.com>
2023-06-19 19:33:45 +0700

    init commit

---
+++ database/migrations/2023_06_18_113658_create_rules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->string('kode_rule')->primary();
+            $table->string('profil');
+            $table->string('jawaban_1');
+            $table->foreign('jawaban_1')->references('kode_jawaban')->on('jawaban');
+            $table->string('jawaban_2');
+            $table->foreign('jawaban_2')->references('kode_jawaban')->on('jawaban');
+            $table->string('jawaban_3');
+            $table->foreign('jawaban_3')->references('kode_jawaban')->on('jawaban');
+            $table->string('jawaban_4');
+            $table->foreign('jawaban_4')->references('kode_jawaban')->on('jawaban');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rules');
+    }
+};
